0bfd496442eedf2eaa06ceaf2e08fc5871b54586
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2024-09-27 21:58:47 -0400

    `switch-bsp`: update headers to match previous commit

--- deb_package_sources/noble/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
+++ deb_package_sources/noble/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Sat Aug 31 19:25:58 EDT 2024"
+#define UTS_VERSION "#5 SMP PREEMPT Thu Sep 26 11:18:43 EDT 2024"
 #define LINUX_COMPILE_BY "theofficialgman"
 #define LINUX_COMPILE_HOST "buildbot"
 #define LINUX_COMPILER "gcc version 7.5.0 (Linaro GCC 7.5-2019.12) "
