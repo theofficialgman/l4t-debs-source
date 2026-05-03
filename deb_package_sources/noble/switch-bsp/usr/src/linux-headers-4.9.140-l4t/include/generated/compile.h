788867730f12e45e8b273c36c372cbc21a82de03
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-05-02 20:34:10 -0400

    switch-bsp and switch-dock-handler: simplify kernel changes and simplify dock-hotplug resolution setting
    the switch handheld display still rotated in hardware as part of the DC kernel driver from the previous commit. however it is now exposed and recorded as portrait for framebuffer purposes. it is exposed as 1280x720 for the codepath used by the Xorg (and likely other) userspace GPU driver.
    
    This resolves the previous issue with pure framebuffer mode. Those will continue to stay portrait.

--- deb_package_sources/noble/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
+++ deb_package_sources/noble/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 48 */
+/* This file is auto generated, version 130 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#48 SMP PREEMPT Sun Apr 26 00:04:38 EDT 2026"
+#define UTS_VERSION "#130 SMP PREEMPT Sat May 2 19:23:50 EDT 2026"
 #define LINUX_COMPILE_BY "theofficialgman"
 #define LINUX_COMPILE_HOST "buildbot"
 #define LINUX_COMPILER "gcc version 7.5.0 (Linaro GCC 7.5-2019.12) "
