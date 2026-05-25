e2b6e205a1491d0c70c8df4e956c31d2f8dfff01
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-05-25 11:33:28 -0400

    switch-bsp: fix potential framecap to native display rate when handheld

--- deb_package_sources/jammy/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
+++ deb_package_sources/jammy/switch-bsp/usr/src/linux-headers-4.9.140-l4t/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 139 */
+/* This file is auto generated, version 146 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#139 SMP PREEMPT Mon May 4 22:31:12 EDT 2026"
+#define UTS_VERSION "#146 SMP PREEMPT Mon May 25 11:21:56 EDT 2026"
 #define LINUX_COMPILE_BY "theofficialgman"
 #define LINUX_COMPILE_HOST "buildbot"
 #define LINUX_COMPILER "gcc version 7.5.0 (Linaro GCC 7.5-2019.12) "
